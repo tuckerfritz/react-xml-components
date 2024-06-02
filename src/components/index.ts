b6fc012a361c012b40afd4d1cbeed9e7133808c8
Tucker Fritz <tuckerpfritz@gmail.com>
2024-06-02 15:10:42 -0500

    Namespace the Xml components.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,24 @@
-export * from "./XmlAttribute";
-export * from "./XmlEditor";
-export * from "./XmlElement";
-export * from "./XmlInput";
-export * from "./XmlSelect";
-export * from "./XmlText";
-export * from "./XmlTextarea";
+export { type XmlEditorRef } from "./XmlEditor";
+
+import XmlAttribute from "./XmlAttribute";
+import XmlEditor from "./XmlEditor";
+import XmlElement from "./XmlElement";
+import XmlInput from "./XmlInput";
+import XmlSelect from "./XmlSelect";
+import XmlText from "./XmlText";
+import XmlTextarea from "./XmlTextarea";
+
+const Xml = Object.assign(
+  {},
+  {
+    Attribute: XmlAttribute,
+    Root: XmlEditor,
+    Element: XmlElement,
+    Input: XmlInput,
+    Select: XmlSelect,
+    Text: XmlText,
+    Textarea: XmlTextarea,
+  },
+);
+
+export default Xml;
