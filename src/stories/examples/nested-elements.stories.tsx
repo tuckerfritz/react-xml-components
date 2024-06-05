213744826b0cd5a879fc0706b94eea3701e5a873
Tucker Fritz <tuckerpfritz@gmail.com>
2024-06-04 20:24:02 -0500

    Add example of nested element.

---
+++ src/stories/examples/nested-elements.stories.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from "react";
+import { XML, XMLDocumentRefType, Input } from "@src/components";
+import XMLViewer from "react-xml-viewer";
+import "../stories.css";
+
+export function NestedElement() {
+  const editorRef = useRef<XMLDocumentRefType>(null);
+  const [xml, setXml] = useState(
+    `<nested><element>Please edit me!</element></nested>`,
+  );
+
+  const handleSubmit = () => {
+    if (editorRef.current) {
+      const updatedXml = editorRef.current.getXmlString();
+      setXml(updatedXml);
+    }
+  };
+
+  return (
+    <div className="example">
+      <form
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <XML ref={editorRef} initialDoc={xml}>
+          <XML.Root>
+            <XML.Element name="nested">
+              <XML.Element name="element">
+                <Input />
+              </XML.Element>
+            </XML.Element>
+          </XML.Root>
+        </XML>
+        <button type="submit">submit</button>
+      </form>
+      <XMLViewer xml={xml} />
+    </div>
+  );
+}
+
+export default {
+  title: "Examples",
+};
